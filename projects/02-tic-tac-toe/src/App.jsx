4bf8442069c131b1c440365afa2e5e5929856ffb
Bryan Isrrael Gavilanez Bacilio <bigavilanezb00@gmail.com>
2025-03-22 21:05:25 +0100

    Add standard lint

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -3,7 +3,7 @@ import confetti from "canvas-confetti"
 
 import { Square } from "./components/Square.jsx"
 import { TURNS } from "./constants.js"
-import { checkWinnerFrom } from "./logic/board.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
 import { saveGameToStorage, resetGameToStorage } from "./logic/storage/index.js"
 
