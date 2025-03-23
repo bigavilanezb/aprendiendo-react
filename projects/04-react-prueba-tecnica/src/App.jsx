463febe656b81e6222332a8f1ba9231dbda8f06b
Bryan Isrrael Gavilanez Bacilio <bigavilanezb00@gmail.com>
2025-03-23 21:42:36 +0100

    Prueba técnica de React

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+// const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  // para recuperar la cita al cargar la página
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threeFirstWord = fact.split(' ', 3).join(' ')
+        console.log(threeFirstWord)
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+            console.log(url)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
+  )
+}
